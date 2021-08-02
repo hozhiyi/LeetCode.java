d7da159c7d8995ab8fee55a077d8400be7cb20f7
Ho Zhi Yi <hozhiyi13@gmail.com>
2021-08-02 11:14:22 +0800

    611 ValidTriangleNumber

---
+++ src/_202107_JulyLeetCodingChallenge/_611_ValidTriangleNumber.java
@@ -0,0 +1,24 @@
+package _202107_JulyLeetCodingChallenge;
+
+import java.util.Arrays;
+
+public class _611_ValidTriangleNumber {
+    public int triangleNumber(int[] nums) {
+        Arrays.sort(nums);
+        int n = nums.length;
+        int res = 0;
+        for (int i = n - 1; i >= 2; --i) {
+            int l = 0, r = i - 1;
+            while (l < r) {
+                if (nums[l] + nums[r] > nums[i]) {
+                    res += r - l;
+                    --r;
+                } else {
+                    ++l;
+                }
+            }
+        }
+        return res;
+    }
+}
+// https://leetcode-cn.com/problems/valid-triangle-number/solution/ming-que-tiao-jian-jin-xing-qiu-jie-by-jerring/

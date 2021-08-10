e241e2fa3623c8e1b6e74539f01a661f71ba41db
Ho Zhi Yi <hozhiyi13@gmail.com>
2021-08-10 16:23:55 +0800

    926 Flip String To Monotone Increasing

--- src/_202108_AugustLeetCodingChallenge/_926_FlipStringToMonotoneIncreasing.java
+++ src/_202108_AugustLeetCodingChallenge/_926_FlipStringToMonotoneIncreasing.java
@@ -3,18 +3,23 @@ package _202108_AugustLeetCodingChallenge;
 public class _926_FlipStringToMonotoneIncreasing {
     class Solution {
         public int minFlipsMonoIncr(String s) {
-            int l = s.length();
-            int[] tracker = new int[l + 1];
+            char[] ch = s.toCharArray();
+            int onescount = 0;
+            int flipcount = 0;
 
-            for (int i = 0; i < l; i++) {
-                tracker[i + 1] = tracker[i] + (s.charAt(i) == '1' ? 1 : 0);
+            for (int i = 0; i < s.length(); i++) {
+                if (ch[i] == '0') {
+                    if (onescount == 0) continue;
+                    flipcount++;
+                } else {
+                    onescount++;
+                }
+                if (flipcount > onescount) {
+                    flipcount = onescount;
+                }
             }
 
-            int res = Integer.MAX_VALUE;
-            for (int i = 0; i < l; i++) {
-                res = Math.min(res, tracker[i] + l - i - (tracker[l] - tracker[i]));
-            }
-            return res;
+            return flipcount;
         }
     }
 }

f9f28ae8e6c1b9568f3b86c1a8ec1f5930ee1aeb
Ho Zhi Yi <hozhiyi13@gmail.com>
2021-08-13 20:01:11 +0800

    954 Array Of Doubled airs

--- src/_202108_AugustLeetCodingChallenge/_954_ArrayOfDoubledPairs.java
+++ src/_202108_AugustLeetCodingChallenge/_954_ArrayOfDoubledPairs.java
@@ -1,31 +1,34 @@
 package _202108_AugustLeetCodingChallenge;
 
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.HashMap;
-import java.util.Map;
-
 public class _954_ArrayOfDoubledPairs {
     class Solution {
         public boolean canReorderDoubled(int[] arr) {
+            if (arr == null || arr.length == 0) return true;
+            int max = Integer.MIN_VALUE;
+            for (int i : arr) max = Math.max(max, Math.abs(i));
 
-            Map<Integer, Integer> count = new HashMap();
-            for (int x : arr)
-                count.put(x, count.getOrDefault(x, 0) + 1);
+            int[] neg = new int[max + 1];
+            int[] pos = new int[max + 1];
 
-            Integer[] B = new Integer[arr.length];
-            for (int i = 0; i < arr.length; ++i)
-                B[i] = arr[i];
-            Arrays.sort(B, Comparator.comparingInt(Math::abs));
+            for (int i = 0; i < arr.length; i++) {
+                if (arr[i] >= 0) pos[arr[i]]++;
+                else neg[Math.abs(arr[i])]++;
+            }
 
-            for (int x : B) {
+            if (pos[0] % 2 != 0) return false;
 
-                if (count.get(x) == 0) continue;
+            for (int i = 1; i < pos.length; i++) {
+                if (pos[i] > 0) {
+                    if (i * 2 >= pos.length || pos[2 * i] < pos[i]) return false;
 
-                if (count.getOrDefault(2 * x, 0) <= 0) return false;
+                    pos[2 * i] -= pos[i];
+                }
 
-                count.put(x, count.get(x) - 1);
-                count.put(2 * x, count.get(2 * x) - 1);
+                if (neg[i] > 0) {
+                    if (i * 2 >= neg.length || neg[2 * i] < neg[i]) return false;
+
+                    neg[2 * i] -= neg[i];
+                }
             }
 
             return true;

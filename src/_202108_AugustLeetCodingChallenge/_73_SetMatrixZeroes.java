ddf02fc441572fe8bf861372b4c1ee14f064a308
Ho Zhi Yi <hozhiyi13@gmail.com>
2021-08-14 01:39:40 +0800

    73 Set Matrix Zeroes

--- src/_202108_AugustLeetCodingChallenge/_73_SetMatrixZeroes.java
+++ src/_202108_AugustLeetCodingChallenge/_73_SetMatrixZeroes.java
@@ -3,25 +3,35 @@ package _202108_AugustLeetCodingChallenge;
 public class _73_SetMatrixZeroes {
     class Solution {
         public void setZeroes(int[][] matrix) {
-            int m = matrix.length, n = matrix[0].length;
-            boolean flagCol = false;
-            for (int i = 0; i < m; i++) {
+            boolean firstColumn = false;
+            for (int i = 0; i < matrix.length; i++) { // rows
                 if (matrix[i][0] == 0) {
-                    flagCol = true;
+                    firstColumn = true;
                 }
-                for (int j = 1; j < n; j++) {
+                for (int j = 1; j < matrix[0].length; j++) { // columns
                     if (matrix[i][j] == 0) {
-                        matrix[i][0] = matrix[0][j] = 0;
+                        matrix[i][0] = 0;
+                        matrix[0][j] = 0;
                     }
                 }
             }
-            for (int i = m - 1; i >= 0; i--) {
-                for (int j = 1; j < n; j++) {
+
+            for (int i = 1; i < matrix.length; i++) {
+                for (int j = 1; j < matrix[0].length; j++) {
                     if (matrix[i][0] == 0 || matrix[0][j] == 0) {
                         matrix[i][j] = 0;
                     }
                 }
-                if (flagCol) {
+            }
+            // set first row to zero
+            if (matrix[0][0] == 0) {
+                for (int i = 0; i < matrix[0].length; i++) {
+                    matrix[0][i] = 0;
+                }
+            }
+            // set first column to zero
+            if (firstColumn) {
+                for (int i = 0; i < matrix.length; i++) {
                     matrix[i][0] = 0;
                 }
             }

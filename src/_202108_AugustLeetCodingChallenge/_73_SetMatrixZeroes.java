5afb84c8e8f463e501e8bae5e3b3ecc0c71df79b
Ho Zhi Yi <hozhiyi13@gmail.com>
2021-08-14 01:32:23 +0800

    73 Set Matrix Zeroes

---
+++ src/_202108_AugustLeetCodingChallenge/_73_SetMatrixZeroes.java
@@ -0,0 +1,30 @@
+package _202108_AugustLeetCodingChallenge;
+
+public class _73_SetMatrixZeroes {
+    class Solution {
+        public void setZeroes(int[][] matrix) {
+            int m = matrix.length, n = matrix[0].length;
+            boolean flagCol = false;
+            for (int i = 0; i < m; i++) {
+                if (matrix[i][0] == 0) {
+                    flagCol = true;
+                }
+                for (int j = 1; j < n; j++) {
+                    if (matrix[i][j] == 0) {
+                        matrix[i][0] = matrix[0][j] = 0;
+                    }
+                }
+            }
+            for (int i = m - 1; i >= 0; i--) {
+                for (int j = 1; j < n; j++) {
+                    if (matrix[i][0] == 0 || matrix[0][j] == 0) {
+                        matrix[i][j] = 0;
+                    }
+                }
+                if (flagCol) {
+                    matrix[i][0] = 0;
+                }
+            }
+        }
+    }
+}

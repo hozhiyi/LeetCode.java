e5a8895cb40c05e7b699d7f735a17669efbf8a24
Ho Zhi Yi <hozhiyi13@gmail.com>
2021-08-25 13:32:35 +0800

    537 Complex Number Multiplication

---
+++ src/_202108_AugustLeetCodingChallenge/_537_ComplexNumberMultiplication.java
@@ -0,0 +1,16 @@
+// Input: num1 = "1+1i", num2 = "1+1i"
+// Output: "0+2i"
+// Explanation: (1 + i) * (1 + i) = 1 + i2 + 2 * i = 2i, and you need convert it to the form of 0+2i.
+package _202108_AugustLeetCodingChallenge;
+
+public class _537_ComplexNumberMultiplication {
+    public String complexNumberMultiply(String a, String b) {
+        String x[] = a.split("\\+|i");
+        String y[] = b.split("\\+|i");
+        int a_real = Integer.parseInt(x[0]);
+        int a_img = Integer.parseInt(x[1]);
+        int b_real = Integer.parseInt(y[0]);
+        int b_img = Integer.parseInt(y[1]);
+        return (a_real * b_real - a_img * b_img) + "+" + (a_real * b_img + a_img * b_real) + "i";
+    }
+}
